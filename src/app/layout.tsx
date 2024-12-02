bdfb580d1c416a082e36477ffedfb317a7bad61a
nattanjunior <fnatanieljunior@gmail.com>
2024-12-02 14:18:21 -0300

    add component Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +28,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        cz-shortcut-listen="true"
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
